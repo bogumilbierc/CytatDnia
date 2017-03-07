2abab9fc82ec4a29b5bdfee33d8568f4543e29e2
Bogumił Bierć <bogumil.bierc@gmail.com>
2017-03-07 19:53:07 +0100

    Alarm interval adjusted.

--- app/src/main/java/pl/jcrusader/cytatdnia/schedule/AlarmSetter.java
+++ app/src/main/java/pl/jcrusader/cytatdnia/schedule/AlarmSetter.java
@@ -5,7 +5,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.SystemClock;
-import android.util.Log;
 
 /**
  * Created by bogumil on 3/7/17.
@@ -13,12 +12,31 @@ import android.util.Log;
 
 public class AlarmSetter {
 
+    private final static Integer INTERVAL_IN_HOURS = 4;
+    private final static Integer MILISECONDS_IN_THIRTY_SECONDS = 30 * 1000;
+
+    /**
+     * Starts (schedules) new alarm. Alarm will be broadcasted to {@link AlarmReceiver} class.
+     * It is set to {@link AlarmManager#ELAPSED_REALTIME} type with first invocation 30 seconds after.
+     * Next invocations will have 4 hours interval.
+     *
+     * @param context context of intent, that starts the alarm
+     */
     public static void startAlarm(Context context) {
         Intent alarmIntent = new Intent(context, AlarmReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent, 0);
         AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        int interval = 1000 * 20 * 1;
-        manager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + 5 * 1000, interval, pendingIntent);
-        Log.i("Alarm set", "AlarmSetter");
+        int interval = getMiliseconds(INTERVAL_IN_HOURS);
+        manager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + MILISECONDS_IN_THIRTY_SECONDS, interval, pendingIntent);
+    }
+
+    /**
+     * Gets number of miliseconds in given amount of hours.
+     *
+     * @param hours number of hours
+     * @return number of miliseconds in given amount of hours
+     */
+    private static int getMiliseconds(int hours) {
+        return 1000 * 60 * 60 * hours;
     }
 }
