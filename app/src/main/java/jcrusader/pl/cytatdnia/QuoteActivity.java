4825bc7858d8c26f33d8876ba02b63716ed306e6
Bogumił Bierć <bogumil.bierc@gmail.com>
2017-03-07 11:27:27 +0100

    Showing notification after downloading quote - that is a subject of change, it will be done in IntentService.

--- app/src/main/java/jcrusader/pl/cytatdnia/QuoteActivity.java
+++ app/src/main/java/jcrusader/pl/cytatdnia/QuoteActivity.java
@@ -1,7 +1,13 @@
 package jcrusader.pl.cytatdnia;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -20,6 +26,7 @@ public class QuoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quote);
         assignViewsToObjects();
         downloadAndDisplayQuote();
+        showNewQuoteNotification();
     }
 
     private void assignViewsToObjects() {
@@ -49,6 +56,40 @@ public class QuoteActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialoginterface, int i) {
                     }
                 }).show();
+    }
+
+    private void showNewQuoteNotification() {
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.notificationTitle));
+
+
+
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, QuoteActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(QuoteActivity.class);
+
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        notificationBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(0, notificationBuilder.build());
     }
 
 }
